a42cb7def8851b87ae55956cf82f29ca53936955
hubert200-lang <hubertche10@gmail.com>
2025-07-23 14:42:58 +0100

    i have added a file

---
+++ 2.0 Software1.0/python basics/day 1/variable.py
@@ -0,0 +1,44 @@
+# print("Hello, Deepseeds")
+# name="Gita"
+
+# print("I am GIta, I love this name -Gita, it was given to me by my father, Paul Gita and i realy like the name Gita")
+# print("my name is "+ name +"was given to me by my father, "+ name +"Paul")
+
+# # numbers
+# whole_number=42
+# decimal_number=3.14519
+# complex_number=2+3j
+
+# #tex
+# greeting="hello world!"    #string (str)
+# single_char='A'            #also a string
+
+# #Boolean (true/false)
+# its_rainny='true'
+# its_sunny='false'
+
+
+# A ban name generator: IT collects your name, agae, favourite meal, school, department,bestfriend and generates a story based on the data collected
+
+name=input("Che Hubert")
+age=input("21")
+fav_meal=input("a meal")
+school=input("coltech")
+department=input("data science")
+best_friend=input("someone")
+
+print(f"My name is {name}")
+
+# simple calculation, calculator
+L=10
+W=5
+A=L*W
+P=2* L*W 
+print(f'the area is {A} and the perimeter is {P}')
+
+
+
+temp_in_celsius=input("enter temp in celsius")
+converted_temp=float(temp_in_celsius)
+results=(converted_temp*9/5)+32;
+print(f"My results: {results}")
